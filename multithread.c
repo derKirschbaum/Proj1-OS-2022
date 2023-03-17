224101f5c44a7d7d555f397b6ed2c1f4b3a8529f
MonikaElisch <125794798+MonikaElisch@users.noreply.github.com>
2023-03-17 21:00:41 +0700

    Update multithread.c

--- multithread.c
+++ multithread.c
@@ -1,6 +1,11 @@
 #include "stdio.h"
 #include "stdlib.h"
 
+
+#define MAX_WRITER 10
+#define MAX_READER 10
+#define MAX_INTEGER 100000
+
 int main(int argc, char* argv[]){
 
     // Check input arguments
@@ -9,13 +14,37 @@ int main(int argc, char* argv[]){
     // !! default value of argc is 1, therefore we need to check for 7 args
     // !! default value of argv[0] is the name of program, custom args starts from argv[1]
     // !! argv = [ programName, # writer, # reader, input, output, seed, N]; # of args = 7
-    if (argc != 7 || argv[1] > 10 || argv[2] > 10 || argv[1] <= 0 || argv[2] <= 0 || argv[5] <= 0 || argv[5] > 100000)
+    if (argc != 7 )
     {
         //Print to stderr (Standard Error)
         fprintf(stderr,
-        "\nInvalid arguments\n\nUsage: ./multithread <# of writers> <# of readers> <input> <output> <seed> <N>\n\nConstraints: 0 < #writer/reader <= 10\n             0 < N <= 100,000");
-        return 1;
+        "Invalid arguments\n\nUsage: ./multithread <# of writers> <# of readers> <input> <output> <seed> <N>);
+        exit(-1);
     }
+    if(atoi(argv[1]) < 1 || MAX_WRITER < atoi(argv[1])){
+        fprintf(stderr,
+        "Invalid arguments\n# of writers must be from 1 - %d",MAX_WRITER);
+        exit(-1);
+    }
+    if(atoi(argv[2]) < 1 || MAX_READER < atoi(argv[2])){
+        fprintf(stderr,
+        "Invalid arguments\n# of readers must be from 1 - %d",MAX_READER);
+        exit(-1);
+    }
+                
+    //-----------------------------------------------
+    if(atoi(argv[6]) < 1 || MAX_INTEGER < atoi(argv[6])){
+        fprintf(stderr,
+        "Invalid arguments\nAmount of integers must be from 1 - %d",MAX_INTEGER);
+        exit(-1);
+    }
+
+    int num_writers =atoi(argv[1]);
+    int num_readers =atoi(argv[2]);
+    
+    int seed =atoi(argv[5]);
+    int N =atoi(argv[6]);
+    
 
     return 0;
 }
